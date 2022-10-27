6b02ea43b1259d713c03bf3f2f43dafc033e6c11
Suvaditya Mukherjee <suvadityamuk@gmail.com>
2022-10-27 12:42:10 +0530

    changes
    Signed-off-by: Suvaditya Mukherjee <suvadityamuk@gmail.com>

--- connector.py
+++ connector.py
@@ -1,4 +1,5 @@
-import MySQLdb
+# import MySQLdb
+import mysql.connector
 from env import mysqlConfig
 
 def executeReadQuery(query, data=None):
@@ -7,18 +8,26 @@ def executeReadQuery(query, data=None):
         Returns:
             - List object as a JSON result from the database
         Warning:
-            - No middleware functions or work to be done here, this is only for returning 
+            - No middleware functions or work to be done here, this is only for returning
             data from the table. All other processes to be done in separate file
     '''
     # mydb_connection.reconnect()
-    mydb_connection = MySQLdb.connect(
+
+    # mydb_connection = mysql.connector.connect(
+    #     host=mysqlConfig["MYSQL_ADDON_HOST"],
+    #     user=mysqlConfig["MYSQL_ADDON_USER"],
+    #     passwd=mysqlConfig["MYSQL_ADDON_PASSWORD"],
+    #     db=mysqlConfig["MYSQL_ADDON_DB"],
+    #     port=mysqlConfig["MYSQL_ADDON_PORT"]
+    # )
+    mydb_connection = mysql.connector.connect(
         host=mysqlConfig["MYSQL_ADDON_HOST"],
         user=mysqlConfig["MYSQL_ADDON_USER"],
-        passwd=mysqlConfig["MYSQL_ADDON_PASSWORD"],
-        db=mysqlConfig["MYSQL_ADDON_DB"]
+        password=mysqlConfig["MYSQL_ADDON_PASSWORD"],
+        database=mysqlConfig["MYSQL_ADDON_DB"],
+        port=3306
     )
     cursor = mydb_connection.cursor()
-    print('Cursor connection established')
     cursor.execute(query, data)
     print('Query executed')
     result = [i for i in cursor]
@@ -32,15 +41,23 @@ def executeWriteQuery(query, data=None):
         Returns:
             - List object as a JSON result from the database
         Warning:
-            - No middleware functions or work to be done here, this is only for returning 
+            - No middleware functions or work to be done here, this is only for returning
             data from the table. All other processes to be done in separate file
     '''
     # mydb_connection.reconnect()
-    mydb_connection = MySQLdb.connect(
+    # mydb_connection = mysql.connector.connect(
+    #     host=mysqlConfig["MYSQL_ADDON_HOST"],
+    #     user=mysqlConfig["MYSQL_ADDON_USER"],
+    #     passwd=mysqlConfig["MYSQL_ADDON_PASSWORD"],
+    #     db=mysqlConfig["MYSQL_ADDON_DB"],
+    #     port=mysqlConfig["MYSQL_ADDON_PORT"]
+    # )
+    mydb_connection = mysql.connector.connect(
         host=mysqlConfig["MYSQL_ADDON_HOST"],
         user=mysqlConfig["MYSQL_ADDON_USER"],
-        passwd=mysqlConfig["MYSQL_ADDON_PASSWORD"],
-        db=mysqlConfig["MYSQL_ADDON_DB"]
+        password=mysqlConfig["MYSQL_ADDON_PASSWORD"],
+        database=mysqlConfig["MYSQL_ADDON_DB"],
+        port=mysqlConfig["MYSQL_ADDON_PORT"]
     )
     cursor = mydb_connection.cursor()
     print('Cursor connection established')
